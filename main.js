ef2ccb6e1a75f058b60619abf6b8a6dfb939f276
Meta77 <horokusa@keio.jp>
2025-09-07 21:49:39 +0900

    コメント　ルートコンポーネント

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ import TaskList from './components/TaskList.js';
 import TaskCard from './components/TaskCard.js';
 
 // Vueアプリケーションを作成
-const app = Vue.createApp(KanbanBoard);
+const app = Vue.createApp(KanbanBoard); // 一番の親、ベースとなるコンポーネントの登録。「ルートコンポーネント」と呼ぶ。
+
 
 // 各コンポーネントをグローバルに登録
 // これにより、どのコンポーネントのテンプレート内でもこれらのタグが使えるようになる
